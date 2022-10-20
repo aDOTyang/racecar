f7e8c96fbf05e33a3f633af9d65cd1f6bb44f4db
aDOTyang <111149139+aDOTyang@users.noreply.github.com>
2022-10-19 21:55:58 -0400

    Revert "Merge branch 'main' of https://github.com/aDOTyang/racecar"
    This reverts commit ef3e40278ed20276041a229e16e5bee1f37478b9, reversing
    changes made to 101fd2a17977fbbbf053683612e826ac52540911.

--- js/site.js
+++ js/site.js
@@ -1,26 +1,90 @@
-function getValues() {
+function palindrome() {
+  // classList displays all HTML classes as array in javascript
   document.getElementById("alert").classList.add("invisible");
-
   let userString = document.getElementById("userString").value;
-  let revString = ReverseAString(userString);
 
-  displayString(revString);
+  if (userString == "") {
+    Swal.fire({
+      icon: "error",
+      backdrop: false,
+      title: "RacecaR",
+      text: "You jumped the light! No input detected.",
+    });
+  } else {
+    // let revString = ReverseString(userString);
+    //displayString(revString, userString);
+
+    let returnObj = checkPalindrome(userString);
+    displayString(returnObj);
+  }
 }
 
-function ReverseAString(userString) {
-  let revString = [];
+function checkPalindrome(userString) {
+  userString = userString.toLowerCase();
+  const regex = /[^a-z0-9]/gi;
+  userString = userString.replace(regex, "");
 
-  // reverse the string using decrementing for loop
+  let revString = [];
+  let returnObj = {};
+
+  // reverse string using for loop
   for (let index = userString.length - 1; index >= 0; index--) {
     revString += userString[index];
   }
-  return revString;
+
+  if (userString == revString) {
+    returnObj.msg = "Yes! This is a palindrome!";
+  } else {
+    returnObj.msg = "No! Not a palindrome!";
+  }
+  returnObj.reversed = revString;
+  return returnObj;
 }
 
-function displayString(revString) {
+// reverse string fn
+// function ReverseString(userString) {
+//   // if using push, must convert array back to string
+//   let revString = [];
+//   for (let i = userString.length - 1; i >= 0; i--) {
+//     revString += userString[i];
+//   }
+//   return revString;
+// }
+
+// quick reverse string fn with JS
+// function ReverseStringJS(userString) {
+//   // 'split' turns user input string into array
+//   let userArray = userString.split("");
+//   // 'reverse' only works on arrays
+//   let revArray = userArray.reverse(userArray);
+//   // 'join' returns the array to a string
+//   let revString = revArray.join("");
+
+//   // lets revString be seen outside of the ReverseStringJS function
+//   return revString;
+// }
+
+// display the string
+function displayString(returnObj) {
+  let palAlert = document.getElementById("alert");
+  let alertHdr = document.getElementById("alertHeader");
+
+  if (returnObj.msg.includes("Not")) {
+    palAlert.classList.remove("alert-success");
+    palAlert.classList.add("alert-danger");
+  } else {
+    palAlert.classList.remove("alert-danger");
+    palAlert.classList.add("alert-success");
+  }
+
+  // write to the page
+  alertHdr.innerHTML = returnObj.msg;
+
   document.getElementById(
     "msg"
-  ).innerHTML = `Your string reversed is: <strong><h4>${revString}</h4></strong>`;
+  ).innerHTML = `Your string reversed is: <br><h4>${returnObj.reversed}</h4>`;
 
-  document.getElementById("alert").classList.remove("invisible");
+  palAlert.classList.remove("invisible");
+
+  // document.getElementById("alert").classList.remove("invisible");
 }
